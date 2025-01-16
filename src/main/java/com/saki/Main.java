9d48807664f111ab472c33f92db565050639dd96
saki <574345456@qq.com>
2025-01-16 15:18:47 +0800

    队列练习

---
+++ src/main/java/com/saki/Main.java
@@ -0,0 +1,7 @@
+package com.saki;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/saki/entity/Element.java
@@ -0,0 +1,10 @@
+package com.saki.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Element {
+    private String id;
+}
