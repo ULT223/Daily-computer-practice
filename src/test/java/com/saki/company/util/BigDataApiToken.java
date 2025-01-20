2396c9417e8833c9b40725ff2ad7406edc12ea3d
saki <574345456@qq.com>
2025-01-20 15:14:24 +0800

    获取token工具编写

---
+++ src/test/java/com/saki/company/util/BigDataApiToken.java
@@ -0,0 +1,16 @@
+package com.saki.company.util;
+
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.crypto.digest.MD5;
+
+public class BigDataApiToken {
+
+    public static void main(String[] args) {
+        String apiPrefix = "";
+        String clientSecret = "";
+        String clientId = "";
+        String apiTokenClient = new MD5().digestHex(apiPrefix+ DateUtil.format(DateUtil.date(), DatePattern.NORM_DATE_FORMAT) +  clientSecret);
+        System.out.println(apiTokenClient);
+    }
+}
